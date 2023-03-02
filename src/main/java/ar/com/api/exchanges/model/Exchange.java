2169e47499540999aec8df0f67bf339c2543dfc2
jclagoria <lagoria79@gmail.com>
2023-03-02 07:29:12 -0300

    add logic and files to work with Volumn data of market by id

--- src/main/java/ar/com/api/exchanges/model/Exchange.java
+++ src/main/java/ar/com/api/exchanges/model/Exchange.java
@@ -9,7 +9,7 @@ import lombok.Data;
 
 @Data
 @JsonIgnoreProperties(ignoreUnknown = true)
-public class Exchange extends ExchangeBase implements Serializable {
+public class Exchange extends ExchangeBase {
 
  @JsonProperty("year_established")
  private long yearEstablished;
